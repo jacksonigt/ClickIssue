f68dae82c8fbf755ea4fb379197a283f26a70a1a
Jackson Buck <89856177+jacksonigt@users.noreply.github.com>
2022-11-28 16:22:04 -0800

    Added second display with second set of buttons.
    Reposition and resize both windows.  Both of them have touch input problems.

--- ClickIssue/Assets/MyDisplay.cs
+++ ClickIssue/Assets/MyDisplay.cs
@@ -4,8 +4,23 @@ public class MyDisplay : MonoBehaviour
 {
     void Start()
     {
-        var display = Display.displays[0];
-        display.Activate();
-        display.SetParams(width: 1600, height: 900, x: 320, y: 0);
+
+
+        for(var i = 0; i < 2; i++)
+        {
+            var display = Display.displays[i];
+
+            if(i == 0)
+            {
+                // To change the size and position of primary display you must also activate it by calling Display.displays[0].Activate(0, 0, 0);
+                // https://docs.unity3d.com/2020.3/Documentation/ScriptReference/Display.SetParams.html
+                display.Activate(0,0,0);
+            }
+            else
+            {
+                display.Activate();
+            }
+            display.SetParams(width: 1600, height: 900, x: 300, y: 0);
+        }
     }
 }
